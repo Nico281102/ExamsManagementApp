e42ba514440295ff7c86fdb46812a0cacf39d462
domenicososta <nicososta28@gmail.com>
2023-10-06 14:21:39 +0200

    Funzionalità che permette a insegnanti di gestire il corso che seguono creando prove, implementazione solo front-end

--- App/blueprints/teacher.py
+++ App/blueprints/teacher.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user, login_required
 
 from App.checkFunctions import checkDocente
+from App.db.models.database import Esami
 
 teacher = Blueprint('teacher', __name__, url_prefix='/teacher', template_folder='templates')
 
@@ -10,6 +11,7 @@ teacher = Blueprint('teacher', __name__, url_prefix='/teacher', template_folder=
 @login_required
 @checkDocente
 def teacherPage():
+    print("sono in teacherPage")
     return render_template('teacher/home.html', professore=current_user)
 
 
@@ -17,14 +19,46 @@ def teacherPage():
 @login_required
 @checkDocente
 def visualizzaEsami():
-    return render_template('teacher/appelli.html', user=current_user)
+    #visualizza l'elenco dei corsi tenuti dal professore e le relative prove
+    print("sono in visualizzaEsami")
+    print(current_user.esami)
+    return render_template('teacher/corsi.html', user=current_user, esami=current_user.esami)
 
 
-@teacher.route('/crea_esami')
+@teacher.route('/creaProve', methods=['POST', 'GET'])
 @login_required
 @checkDocente
-def creaEsami():
-    return render_template('teacher/esami.html', user=current_user)
+def creaProve():
+    #la somma dei pesi delle prove relative a un corso deve fare 1.
+    #se il professore non ha ancora creato prove per un corso, deve poterlo fare
+    #se il professore ha già creato prove per un corso, deve poterle modificare
+    #se il professore ha già creato prove per un corso, deve poterle eliminare
+    #non è possibile per problemi di progettazione creare le prove una alla volta, ma solo tutte insieme
+    docente = current_user
+    esame_id = request.form['esame']
+
+    return render_template('teacher/creaProve.html', user=current_user)
+
+@teacher.route('/creaProve', methods=['POST', 'GET'])
+@login_required
+@checkDocente
+def eliminaProve():
+
+
+    return redirect(url_for('teacher.visualizzaEsami'))
+
+
+
+@teacher.route('/visualizzaProve', methods=['POST', 'GET'])
+@login_required
+@checkDocente
+def visualizzaProve():
+
+    esame_id = request.form['esame']
+    esame = Esami.query.get(esame_id)
+    prove = esame.prove
+    print(prove)
+    return render_template('teacher/visualizzaProve.html', user=current_user, prove=prove, esame=esame)
 
 
 
